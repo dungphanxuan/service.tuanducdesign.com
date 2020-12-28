9e901fb3cf7ba1f3e44f74c42b0b7a9f10453360
Tuan Duc Design <69758022+tuanducdesign@users.noreply.github.com>
2020-12-28 10:41:02 +0700

    Update Layout.js

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -38,10 +38,8 @@ export default class Body extends React.Component {
                     <link rel="preconnect" href="https://fonts.gstatic.com"/>
                     )}
                     {(font === 'nunito-sans') ? (
-                    <link rel="preload" href="https://fonts.googleapis.com/css2?family=Nunito+Sans:ital,wght@0,400;0,700;1,400;1,700&display=swap" as="style"/>
                     <link href="https://fonts.googleapis.com/css2?family=Nunito+Sans:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet"/>
                     ) : ((font === 'fira-sans') && (
-                    <link rel="preload" href="https://fonts.googleapis.com/css2?family=Fira+Sans:ital,wght@0,400;0,600;1,400;1,600&display=swap" as="style"/>
                     <link href="https://fonts.googleapis.com/css2?family=Fira+Sans:ital,wght@0,400;0,600;1,400;1,600&display=swap" rel="stylesheet"/>
                     ))}
                     {_.get(this.props, 'data.config.favicon', null) && (
